b1a250bc7d2a6b1ab75c6f836085b7b389c3fb41
CatrineS <catrinesjocrona@gmail.com>
2018-06-09 12:04:19 +0200

    rotation

--- Newest_KingJam/KingGameJam/Assets/Scripts/RotationController.cs
+++ Newest_KingJam/KingGameJam/Assets/Scripts/RotationController.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class RotationController : MonoBehaviour
 {
 
-    float rotationSpeed = 20;
+    float rotationSpeed = 8;
 
     private void OnMouseDrag()
     {
         float rotX = Input.GetAxis("Mouse X") * rotationSpeed * Mathf.Deg2Rad;
         float rotY = Input.GetAxis("Mouse Y") * rotationSpeed * Mathf.Deg2Rad;
 
-        transform.RotateAround(Vector3.up, -rotX);
-        transform.RotateAround(Vector3.right, rotY);
+        //transform.RotateAround(Vector3.up, -rotX);
+        transform.RotateAround(Vector3.forward, -rotX);
     }
 }
