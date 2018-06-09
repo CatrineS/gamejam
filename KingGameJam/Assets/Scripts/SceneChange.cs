2d94a0708515923201818ffb87527d5ba5f05a85
CatrineS <catrinesjocrona@gmail.com>
2018-06-09 10:15:28 +0200

    can change scene

---
+++ KingGameJam/Assets/Scripts/SceneChange.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour {
+
+    public void changeScene(int sceneNum)
+    {
+        SceneManager.LoadScene(sceneNum);
+    }
+	
+}
