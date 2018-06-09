780214255752e1d7d178829e967c7e61301f281b
AxelSavage <axelsavage@gmail.com>
2018-06-09 16:30:00 +0200

    Level1 Prototyp Completed

--- Newest_KingJam/KingGameJam/Assets/Scripts/BlobAgent.cs
+++ Newest_KingJam/KingGameJam/Assets/Scripts/BlobAgent.cs
@@ -1,15 +1,72 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+[RequireComponent(typeof(Rigidbody))]
 public class BlobAgent : MonoBehaviour {
 
     [SerializeField] private Transform[] positions;
+    [SerializeField] private Vector3 jumpDirection = new Vector3(0, 1f, 0);
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private bool jumping = false;
+    [SerializeField] private float moveSpeed = 3f;
+    private int positionIndex = 0;
+    private Rigidbody rb;
 
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     void Update() {
-        transform.LookAt(positions[0].position);
-        transform.Translate(Vector3.forward * 5f * Time.deltaTime);
+
+        transform.LookAt(positions[positionIndex].position);
+        rb.AddRelativeForce(Vector3.forward * moveSpeed, ForceMode.Force);
+  
+        if (jumping)
+        {
+            jumping = false;
+            rb.AddForce(jumpDirection * jumpForce, ForceMode.Impulse);
+        }
     }
+
+    private void JumpTrigger()
+    {
+        jumping = true;
+    }
+
+
+    private void TriggerNextPosition()
+    {
+        positionIndex++;
+    }
+
+    private void RemoveFromList()
+    {
+        FindObjectOfType<CameraFollow>().RemoveFromList(transform);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "JumpingPad")
+        {
+            JumpTrigger();
+        }
+
+        if(other.tag == "PositionTrigger")
+        {
+            TriggerNextPosition();
+        }
+        
+        if(other.tag == "DeathZone")
+        {
+            RemoveFromList();
+        }
+    }
+
+
 }
+  //transform.position = Vector3.MoveTowards(transform.position, positions[0].position, 2f * Time.deltaTime);
+  //  transform.Translate(Vector3.forward * 5f * Time.deltaTime);
---
+++ Newest_KingJam/KingGameJam/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    [SerializeField] private List<Transform> blobs;
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private Vector3 velocity = Vector3.zero;
+    [SerializeField] private float smoothTime = 0.3f;
+
+    void LateUpdate () {
+        Move();
+
+        print("BlobsCount: " + blobs.Count);
+	}
+
+    private void Move()
+    {
+        Vector3 centerPoint = GetCenterPoint();
+        Vector3 newPosition = centerPoint + offset;
+
+        transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velocity, smoothTime);
+    }
+
+    Vector3 GetCenterPoint()
+    {
+        if(blobs.Count == 1)
+        {
+            return blobs[0].position;
+        }
+
+        Bounds bounds = new Bounds(blobs[0].position, Vector3.zero);
+
+        for(int i = 0; i < blobs.Count; i++)
+        {
+            bounds.Encapsulate(blobs[i].position);
+        }
+
+        return bounds.center;
+    }
+
+
+    public void RemoveFromList(Transform blob)
+    {
+        if(blobs.Count <= 1)
+        {
+            return;
+        }
+        else if(blobs.Contains(blob))
+        {
+            blobs.Remove(blob);
+        }
+    }
+}
