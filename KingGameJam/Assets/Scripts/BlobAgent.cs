ae93a57da6f2ba47a3096ca7a248be3f927d0f8b
AxelSavage <axelsavage@gmail.com>
2018-06-09 10:33:34 +0200

    Added SkyBox

---
+++ KingGameJam/Assets/Scripts/BlobAgent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BlobAgent : MonoBehaviour {
+
+    NavMeshAgent blobAgent;
+
+
+    [SerializeField] private Transform[] positions;
+    private int currentIndex = 0;
+
+
+    private void Awake()
+    {
+        blobAgent = GetComponent<NavMeshAgent>();
+    }
+	
+	void Update () {
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            print("wow");
+            if (currentIndex > positions.Length)
+                return;
+
+
+            blobAgent.SetDestination(positions[currentIndex].position);
+            currentIndex++;
+        }
+	}
+}
