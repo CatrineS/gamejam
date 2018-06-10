5bb265c7dc7031779c5ee50a566bd06cb41a76f3
CatrineS <catrinesjocrona@gmail.com>
2018-06-10 15:55:36 +0200

    main

--- Newest_KingJam/TestGameJam/Assets/Scripts/PlayButtonTwirl.cs
+++ Newest_KingJam/TestGameJam/Assets/Scripts/PlayButtonTwirl.cs
@@ -1,22 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayButtonTwirl : MonoBehaviour
 {
     private Vector3 screenPoint;
     private float rotationSpeed = 5f;
+    private int counter = 0;
+    public Text helpText;
+
 
     private void OnMouseDrag()
     {
+        
         float mouseXInput = Input.GetAxis("Mouse X") * rotationSpeed * Mathf.Deg2Rad;
         float mouseYInput = Input.GetAxis("Mouse Y") * rotationSpeed * Mathf.Deg2Rad;
 
         if (gameObject.tag == "playButton")
         {
             //transform.RotateAround(Vector3.forward, mouseYInput);
+
+        
             transform.RotateAround(Vector3.forward, mouseXInput);
-            Invoke("StartScene", 1.5f);
+
+            Invoke("StartScene", 2f);
         }
     }
 
@@ -25,4 +33,14 @@ public class PlayButtonTwirl : MonoBehaviour
 
         SceneChange.instance.changeScene(1);
     }
+    private void OnMouseDown()
+    {
+        counter++;
+   
+        if (counter >= 2)
+        {
+            helpText.gameObject.SetActive(true);
+        }
+    }
+
 }
