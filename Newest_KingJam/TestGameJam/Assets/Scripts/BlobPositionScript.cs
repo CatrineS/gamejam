1207fed1112252ae3ec5fd6b2e5c3f3930b44534
AxelSavage <axelsavage@gmail.com>
2018-06-10 14:48:03 +0200

    BloblPositionScript Added

---
+++ Newest_KingJam/TestGameJam/Assets/Scripts/BlobPositionScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlobPositionScript : MonoBehaviour {
+
+    public Transform positionParent;
+    public List<Transform> positionTransforms;
+    public Transform currentDestination;
+
+    int currentIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+		foreach(Transform child in positionParent)
+        {
+            positionTransforms.Add(child);
+        }
+	}
+	
+    public void TriggerNextPosition()
+    {
+        currentDestination = positionTransforms[currentIndex];
+        currentIndex++;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "PositionTrigger")
+        {
+            TriggerNextPosition();
+        }
+    }
+
+}
